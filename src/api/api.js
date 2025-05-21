ffe6e9a1c92123f62d8737743bfd86df4ff722bf
JaimeGR3 <165404895+JaimeGR3@users.noreply.github.com>
2025-05-21 18:46:43 +0200

    recursion and dark mode

--- src/api/api.js
+++ src/api/api.js
@@ -25,9 +25,11 @@ export function uploadAndPredictImage(file, threshold = 0.2) {
         if (response.ok && data && data.prediction !== undefined) {
           const result = data.prediction >= threshold ? "Melanoma" : "No Melanoma";
           const predictionPercentage = (data.prediction * 100).toFixed(2);
-
-          resultText.textContent = `Resultado: ${result}`;
-          console.log(`Resultado: ${result} (Prob: ${predictionPercentage}%)`);
+          const icon = result === "Melanoma" ? melanoma_svg : no_melanoma_svg;
+          resultText.innerHTML = `
+            Resultado: <strong>${result}</strong><br>
+            <span class="inline-block align-middle ml-2">${icon}</span>
+          `;
           resolve(data.prediction);  // <-- Aquí se resuelve la promesa con el valor
         } else {
           resultText.textContent = `Error al procesar la imagen.`;
@@ -47,3 +49,18 @@ export function uploadAndPredictImage(file, threshold = 0.2) {
     reader.readAsDataURL(file);
   });
 }
+
+
+
+const melanoma_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="none" viewBox="0 0 24 24" stroke="#dc2626" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="12" cy="12" r="10" stroke="#dc2626" stroke-width="2" fill="#fee2e2"/>
+  <circle cx="8" cy="10" r="2" fill="#b91c1c"/>
+  <circle cx="15" cy="14" r="3" fill="#b91c1c"/>
+  <path d="M12 8v4" stroke="#b91c1c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <circle cx="12" cy="17" r="1" fill="#b91c1c"/>
+</svg>`
+
+const no_melanoma_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="none" viewBox="0 0 24 24" stroke="#16a34a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="12" cy="12" r="10" stroke="#16a34a" stroke-width="2" fill="#dcfce7"/>
+  <path d="M9 12l2 2 4-4" stroke="#16a34a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
